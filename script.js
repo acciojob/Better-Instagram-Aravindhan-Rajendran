97baa67ea351dbb926ab3332c024acdce4944254
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-06-25 19:35:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
+function drag(event) {
+  event.dataTransfer.setData("text", event.target.id);
 }
 
 // This function is called when something is dragged over the drop target
-function allowDrop(ev) {
-  ev.preventDefault(); // Prevent default handling of the data (default is open as link on drop)
+function allowDrop(event) {
+  event.preventDefault(); // Prevent default handling of the data (default is open as link on drop)
 }
 
 // This function is called when the dragged element is dropped on the target
-function drop(ev) {
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
+function drop(event) {
+  event.preventDefault();
+  var data = event.dataTransfer.getData("text");
   var draggedElement = document.getElementById(data);
-  var dropTarget = ev.target;
+  var dropTarget = event.target;
 
   // Swap background images
   var temp = draggedElement.style.backgroundImage;
