89a6e71d6f7840ceb89943cec6c98a1ee72f22c0
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-06-25 21:13:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-function drag(event) {
-  event.dataTransfer.setData("text", event.target.id);
-}
-function allowDrop(event) {
-  event.preventDefault(); 
-}
 function drop(event) {
   event.preventDefault();
   var data = event.dataTransfer.getData("text");
   var draggedElement = document.getElementById(data);
   var dropTarget = event.target;
+
+  // Swap background images
   var temp = draggedElement.style.backgroundImage;
   draggedElement.style.backgroundImage = dropTarget.style.backgroundImage;
   dropTarget.style.backgroundImage = temp;
+
+  // Swap innerHTML
+  var tempText = draggedElement.innerHTML;
+  draggedElement.innerHTML = dropTarget.innerHTML;
+  dropTarget.innerHTML = tempText;
 }
