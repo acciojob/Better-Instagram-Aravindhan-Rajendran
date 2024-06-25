df308b4a974a6d886edc73a0ffd54e4bec9657b3
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-06-25 18:20:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+// This function is called when something is dragged over the drop target
+function allowDrop(ev) {
+  ev.preventDefault(); // Prevent default handling of the data (default is open as link on drop)
+}
+
+// This function is called when the dragged element is dropped on the target
 function drop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
