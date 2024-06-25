b160bdb0f82686115fcf32b0a037847094e25934
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-06-25 17:18:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your code here
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  var draggedElement = document.getElementById(data);
+  var dropTarget = ev.target;
+
+  // Swap background images
+  var temp = draggedElement.style.backgroundImage;
+  draggedElement.style.backgroundImage = dropTarget.style.backgroundImage;
+  dropTarget.style.backgroundImage = temp;
+}
